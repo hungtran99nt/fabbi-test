23d19c534ee108f5e94bbd9cf4ac0b672fc75a7d
tqh <tranhung99nt@gmail.com>
2024-03-02 08:35:11 +0700

    cicd: update git workflow

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/fabbi-test/',
+  base: '/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
